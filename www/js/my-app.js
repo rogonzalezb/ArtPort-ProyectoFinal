cbadc5b85c5086a06717e96c30ddba05ea830f39
rogonzalezb <ro.gonzalez597@gmail.com>
2021-03-08 05:07:43 -0300

    Registro, ingreso, iconos agregados.

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,44 +1,148 @@
-  
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
 var app = new Framework7({
-    // App root element
-    root: '#app',
-    // App Name
-    name: 'My App',
-    // App id
-    id: 'com.myapp.test',
-    // Enable swipe panel
-    panel: {
-      swipe: 'left',
+  // App root element
+  root: '#app',
+  // App Name
+  name: 'My App',
+  // App id
+  id: 'com.myapp.test',
+  // Enable swipe panel
+  panel: {
+    swipe: 'left',
+  },
+  // Add default routes
+  routes: [{
+      path: '/registro/',
+      url: 'registro.html',
     },
-    // Add default routes
-    routes: [
-      {
-        path: '/about/',
-        url: 'about.html',
-      },
-    ]
-    // ... other parameters
-  });
+    {
+      path: '/ingresar/',
+      url: 'ingresar.html',
+    },
+    {
+      path: '/index/',
+      url: 'index.html',
+    },
+  ]
+  // ... other parameters
+});
 
 var mainView = app.views.create('.view-main');
+var db = firebase.firestore();
+var colUsuarios = db.collection("usuarios");
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
-    console.log("Device is ready!");
+  console.log("Device is ready!");
 });
 
 // Option 1. Using one 'page:init' handler for all pages
-$$(document).on('page:init', function (e) {
-    // Do something here when page loaded and initialized
-    console.log(e);
+$$(document).on('page:init', function(e) {
+  // Do something here when page loaded and initialized
+  console.log(e);
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+$$(document).on('page:init', '.page[data-name="registro"]', function(e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+  console.log("listo!");
+
+  app.navbar.hide('#topNavbar', false);
+  app.toolbar.hide('#botToolbar', false);
+
+  $$('#noArtista').on('click', fnActivar);
+  $$('#artista').on('click', fnActivar2);
+
+  $$('#btnRegistro').on('click', fnRegistrar);
+
+
 })
+
+$$(document).on('page:init', '.page[data-name="ingresar"]', function(e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+  console.log("listo!");
+
+  app.navbar.hide('#topNavbar', false);
+  app.toolbar.hide('#botToolbar', false);
+
+  $$('#btnIngresar').on('click', fnIngresar);
+
+
+})
+
+
+
+function fnActivar() {
+  $$('#noArtista').addClass('activo');
+  $$('#artista').removeClass('activo');
+}
+
+function fnActivar2() {
+  $$('#artista').addClass('activo');
+  $$('#noArtista').removeClass('activo');
+}
+
+function fnRegistrar() {
+
+  rEmail = $$('#regEmail').val();
+  rPass = $$('#regPass').val();
+  rUsua = $$('#regUsuario').val();
+  rFec = $$('#regFecha').val();
+
+  firebase.auth().createUserWithEmailAndPassword(rEmail, rPass)
+    .then(function() {
+      console.log("registro ok");
+      // aca tengo el usuario generado en AUTH
+
+      if ($$('#noArtista').hasClass("activo")) {
+        var tipoUsuario = $$('#noArtista').val();
+      } else if ($$('#artista').hasClass("activo")) {
+        var tipoUsuario = $$('#artista').val();
+      }
+
+      datos = {
+        NombreUsuario: rUsua,
+        TipoUsuario: tipoUsuario,
+        Fecha: rFec
+      };
+      colUsuarios.doc(rEmail).set(datos);
+
+
+    })
+
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode == 'auth/weak-password') {
+        alert('Clave muy débil.');
+      } else {
+        alert(errorCode + "|" + errorMessage);
+      }
+      console.log(error);
+    });
+
+}
+
+function fnIngresar() {
+
+  email = $$('#emailLogin').val();
+  password = $$('#passLogin').val();
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      console.log("ingresaste");
+
+      mainView.router.navigate('/index/');
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorCode);
+    });
+
+}
